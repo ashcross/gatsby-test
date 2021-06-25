60fd4f3252c52312c45923864b3c23efa66a518b
Ash Cross <ash@moca.co.nz>
2021-06-25 20:48:49 +1200

    Finished Gatsby Tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,10 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby Test",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp" //Needed for dyanimc shapes
+  ],
 };
